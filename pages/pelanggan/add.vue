c98b4472e0926c5274dd592c73bd23e3c38c874d
mjumain <mjumain11@gmail.com>
2023-08-17 19:10:05 +0700

    form data pelanggan (index dan add)

--- pages/pelanggan/add.vue
+++ pages/pelanggan/add.vue
@@ -4,82 +4,111 @@
             <div class="ps-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0 p-0">
-                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                        <li class="breadcrumb-item"><nuxt-link to="/"><i class="bx bx-home-alt"></i></nuxt-link>
                         </li>
                         <li class="breadcrumb-item" aria-current="page">
-                            <Nuxt-link to="/pelanggan">Pelanggan</Nuxt-link>
+                            <Nuxt-link to="/pelanggan">Data Pelanggan</Nuxt-link>
                         </li>
-                        <li class="breadcrumb-item active" aria-current="page">Add</li>
+                        <li class="breadcrumb-item active" aria-current="page">Tambah</li>
                     </ol>
                 </nav>
             </div>
         </div>
-
         <div class="row">
-            <div class="col-xl-7 mx-auto">
-                <div class="card border-top border-0 border-4 border-primary">
-                    <div class="card-body p-5">
-                        <div class="card-title d-flex align-items-center">
-                            <div><i class="bx bxs-edit me-1 font-22 text-primary"></i>
+            <form class="row">
+                <div class="col-xl-6 mx-auto">
+                    <div class="card border-top border-0 border-4 border-primary">
+                        <div class="card-body">
+                            <div class="card-title d-flex align-items-center">
+                                <div><i class="bx bxs-edit me-1 font-22 text-primary"></i>
+                                </div>
+                                <h5 class="mb-0 text-primary">Tambah Data Pelanggan</h5>
                             </div>
-                            <h5 class="mb-0 text-primary">Tambah Pelanggan</h5>
+                            <hr>
+                            <div class="col-md-12">
+                                <label class="form-label">Nama Depan</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="col-md-12">
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">Nama Belakang</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">Alamat</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">Area</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">Email</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">Nomor Handphone</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">ID Card</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
                         </div>
-                        <hr>
-                        <form class="row g-3">
-                            <div class="col-md-12">
-                                <label class="form-label">Name Pool</label>
-                                <input type="text" class="form-control" v-model="param.pool_name">
-                            </div>
-                            <div class="col-md-12">
-                                <label class="form-label">IP Range</label>
-                                <input type="text" class="form-control" v-model="param.ip">
-                            </div>
-
-                            <div class="col-12">
-                                <a @click="save()" class="btn btn-primary px-5">
-                                    <i class="bx bxs-save me-1 font-22 text-default"></i>
-                                    Simpan
-                                </a>
-                            </div>
-                        </form>
                     </div>
                 </div>
-
-            </div>
+                <div class="col-xl-6 mx-auto">
+                    <div class="card border-top border-0 border-4 border-primary">
+                        <div class="card-body">
+                            <div class="card-title d-flex align-items-center">
+                                <div><i class="bx bxs-edit me-1 font-22 text-primary"></i>
+                                </div>
+                                <h5 class="mb-0 text-primary">Setting Paket</h5>
+                            </div>
+                            <hr>
+                            <div class="col-md-12">
+                                <label class="form-label">User PPPoe</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="col-md-12">
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">Password PPPoe</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">IP Pool</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">Paket Internet</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">Tanggal Register</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <label class="form-label">Masa Aktif (Hari)</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <a @click="save()" class="btn btn-primary px-5 radius-30">
+                        <i class="bx bxs-save me-1 font-22 text-default"></i>
+                        Simpan
+                    </a>
+                </div>
+            </form>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            param: {
-                pool_name: "",
-                ip: ""
-            }
-        }
-    },
-    methods: {
-        async save() {
-            try {
-                let response = await this.$axios.$post(
-                    '/api/ippool',
-                    this.param
-                )
-                // this.$router.push({
-                //     name: 'dashboard-projects-id',
-                //     params: { id: response.data.id },
-                // })
-                console.log(response)
-                this.$toast.info("Sukses simpan data !");
-                this.$router.push('/ip-pool')
-            } catch (error) {
-                console.log(error)
-                this.$toast.error("Gagal simpan data !");
-            }
-        }
-    }
 
 }
 </script>
--- pages/pelanggan/index.vue
+++ pages/pelanggan/index.vue
@@ -1,18 +1,18 @@
 <template>
-<div>
+    <div>
         <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
             <div class="ps-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0 p-0">
-                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                        <li class="breadcrumb-item"><nuxt-link to="/"><i class="bx bx-home-alt"></i></nuxt-link>
                         </li>
-                        <li class="breadcrumb-item active" aria-current="page">Pelanggan</li>
+                        <li class="breadcrumb-item active" aria-current="page">Data Pelanggan</li>
                     </ol>
                 </nav>
             </div>
         </div>
         <div class="card">
-            <div class="card-body">
+            <div class="card-body border-top border-0 border-4 border-primary">
                 <nuxt-link to="/pelanggan/add" class="btn btn-primary px-5"><i class="bx bx-plus mr-1"></i>Tambah</nuxt-link>
                 <br><br>
                 <div class="table-responsive">
@@ -20,32 +20,13 @@
                         <thead>
                             <tr>
                                 <th>No</th>
-                                <th>Kode Pelanggan</th>
-                                <th>Nama Lengkap</th>
-                                <th>Area</th>
-                                <th>Tgl Daftar</th>
-                                <th>Tgl Expired</th>
+                                <th>Name Pool</th>
+                                <th>IP Range</th>
                                 <th>Option</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="(data, index) in pelanggans.data" :key="data.id">
-                                <td>{{ index + 1 }}</td>
-                                <td>{{ data.kode_pelanggan }}</td>
-                                <td>{{ data.nama_depan+' '+data.nama_belakang }}</td>
-                                <td>{{ data.area }}</td>
-                                <td>{{ data.tgl_daftar }}</td>
-                                <td>{{ data.tgl_expired }}</td>
-                                <td>
-                                    <nuxt-link :to="'/pelanggan/edit/' + data.id" class="btn btn-xs btn-primary" title="Edit">
-                                        <i class="bx bx-edit me-0"></i>
-                                    </nuxt-link>
-
-                                    <a @click="deleteData(data.id)" class="btn btn-xs btn-danger" title="Hapus">
-                                        <i class="bx bx-trash me-0"></i>
-                                    </a>
-                                </td>
-                            </tr>
+                            
                         </tbody>
                     </table>
                 </div>
@@ -55,37 +36,5 @@
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            pelanggans: []
-        }
-    },
-    async asyncData({ $axios }) {
-        const pelanggans = await $axios.$get(
-            '/api/pelanggan'
-        )
-        return { pelanggans }
-    },
-    methods: {
-        async deleteData(id) {
-            try {
-                let response = await this.$axios.$delete(
-                    '/api/pelanggan/'+id,
-                    this.param
-                )
-                console.log(response)
-                if (response.code == 200) {
-                    this.$toast.info("Sukses delete data !");
-                } else {
-                    this.$toast.warning(response.status);
-                }
-                window.location.reload()
-            } catch (error) {
-                console.log(error)
-                this.$toast.error("Gagal delete data !");
-            }
-        }
-    }
-}
+export default {}
 </script>
